21b725c518a34aa17ccfb45103d6b5520afe544c
ASUS <ksnehal666@gmail.com>
2022-05-10 14:45:44 +0530

    Connecting functions and buttons

---
+++ android/app/src/main/kotlin/com/example/functions_and_buttons/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.functions_and_buttons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestions()
+  {
+    print('Answer Chosen!');
+  }
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favt color ?',
+      'What\'s your favt animal ?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Second App'),
+        ),
+        body:Column(
+          children: [
+            Text('The Question!'),
+            RaisedButton(child: Text('Answer 1'),onPressed: answerQuestions),
+            RaisedButton(child: Text('Answer 2'),onPressed: answerQuestions),
+            RaisedButton(child: Text('Answer 3'),onPressed: answerQuestions
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
